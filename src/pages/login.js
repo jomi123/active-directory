07df1511045b7a74174f37504825e7ba60fb191a
Jomi Susan Mathew <73014817+jomi123@users.noreply.github.com>
2023-11-05 11:32:57 +0530

    refactor: login page, logo

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,12 @@
 import Stack from "@mui/material/Stack";
-import { Button, TextField, Typography } from "@mui/material";
+import { Button, Card, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import { useLogin, useNotify } from "react-admin";
 
 export const Login = () => {
-  var [email, setEmail] = useState("");
-  var [error, setError] = useState(null);
-  var [password, setPassword] = useState(null);
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(null);
+  const [password, setPassword] = useState(null);
   const login = useLogin();
   const notify = useNotify();
 
@@ -14,7 +14,7 @@ export const Login = () => {
     return /\S+@\S+\.\S+/.test(email);
   }
 
-  var handleOnChangeEmail = (event) => {
+  const handleOnChangeEmail = (event) => {
     if (!validEmail(event.target.value)) {
       setError("Invalid Email");
     } else {
@@ -24,50 +24,57 @@ export const Login = () => {
     setEmail(event.target.value);
   };
 
-  var handleOnChangePassword = (event) => {
+  const handleOnChangePassword = (event) => {
     setPassword(event.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ email, password });
     login({ email, password }).catch(() => notify("Invalid email or password"));
   };
 
   return (
-    <Stack
-      spacing={2}
-      sx={{
-        textAlign: "center",
-        width: "30%",
-        margin: "auto",
-        padding: "2rem",
-        border: "1px solid ",
-        borderColor: "grey.500",
-        borderRadius: "10px",
+    <div
+      style={{
+        height: "45vh",
+        margin: "2rem",
       }}
     >
-      <Typography variant="h3">Sign In</Typography>
-      <TextField
-        required
-        label="Email-Address"
-        type="email"
-        onChange={handleOnChangeEmail}
-      ></TextField>
-      {error && (
-        <Typography variant="h8" style={{ color: "red", textAlign: "left" }}>
-          {error}
-        </Typography>
-      )}
-      <TextField
-        required
-        label="Password"
-        type="password"
-        onChange={handleOnChangePassword}
-      ></TextField>
-      <Button variant="contained" onClick={handleSubmit}>
-        Login
-      </Button>
-    </Stack>
+      <Stack
+        spacing={2}
+        sx={{
+          textAlign: "center",
+          width: "30%",
+          margin: "auto",
+          padding: "2rem",
+          border: "1px solid ",
+          borderColor: "grey.500",
+          borderRadius: "10px",
+          height: "100%",
+        }}
+      >
+        <Typography variant="h3">Sign In</Typography>
+        <TextField
+          required
+          label="Email-Address"
+          type="email"
+          onChange={handleOnChangeEmail}
+        ></TextField>
+        {error && (
+          <Typography variant="h8" style={{ color: "red", textAlign: "left" }}>
+            {error}
+          </Typography>
+        )}
+        <TextField
+          required
+          label="Password"
+          type="password"
+          onChange={handleOnChangePassword}
+        ></TextField>
+        <Button variant="contained" onClick={handleSubmit}>
+          Login
+        </Button>
+      </Stack>
+    </div>
   );
 };
